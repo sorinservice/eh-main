af557b6ba8527777d86c5723b0e51d5a0f3d2c33
sorinservice <nemesisofnocturn@gmail.com>
2025-08-31 00:24:36 +0200

    Update 7711635737.lua

--- mappings/7711635737.lua
+++ mappings/7711635737.lua
@@ -7,14 +7,7 @@
 --  - defaultUnknown ist der Fallback-Text, wenn nichts passt.
 
 return {
-    -- Mapping nach ID (Strings, nicht Zahlen!)
-    byId = {
-        -- ["123456"] = "Iron Sword",
-        -- ["987654321"] = "Healing Wand",
-        -- ["1122334455"] = "Grapple Hook",
-    },
-
-    -- Mapping nach Namen (genauer Instanzname des Tools/Accessories)
+    -- Mapping nach Name (Strings, nicht Zahlen!)
     byName = {
         ["Phone"] = "Phone",
         ["Flashlight"]    = "Flashlight",
