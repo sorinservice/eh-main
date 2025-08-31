b2d5d78170b0334654f728eb5ab3ac183df0116a
sorinservice <nemesisofnocturn@gmail.com>
2025-08-31 10:57:21 +0200

    Update info.lua

--- tabs/info.lua
+++ tabs/info.lua
@@ -1,35 +1,12 @@
 -- tabs/info.lua
 return function(tab, OrionLib)
-    -- Ping-Button
-    tab:AddButton({
-        Name = "Ping-Notification",
-        Callback = function()
-            OrionLib:MakeNotification({
-                Name = "Ping",
-                Content = "Ping erfolgreich",
-                Time = 3
-            })
-        end
-    })
+        -- Info
+    tab:AddParagraph(
+        "Thanks for using SorinHub",
+        "This is the Developer Version. Why are you using this?/n" ..
+        "Its not safe to use. You could be banned!!!"
+    )
 
-    -- Demo: Toggle + kleiner Loop
-    local state = false
-    tab:AddToggle({
-        Name = "AutoJump",
-        Default = false,
-        Callback = function(v) state = v end
-    })
-
-    task.spawn(function()
-        while true do
-            if state then
-                local plr = game.Players.LocalPlayer
-                local char = plr and plr.Character
-                local hum = char and char:FindFirstChildOfClass("Humanoid")
-                if hum then hum.Jump = true end
-            end
-            task.wait(0.5)
-        end
-    end)
-end
-
+    tab:AddParagraph(
+        "Join our Discord"
+    )
