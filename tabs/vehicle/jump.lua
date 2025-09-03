af769f9cf8ba3ce26374b021fc13beb8c83cb54f
sorinservice <nemesisofnocturn@gmail.com>
2025-09-03 19:14:20 +0200

    Update jump.lua

--- tabs/vehicle/jump.lua
+++ tabs/vehicle/jump.lua
@@ -1,1 +1,22 @@
+-- tabs/vehicle/jump.lua
+return function(SV, tab, OrionLib)
+    local notify = SV.notify
+    local J = { power = 220 } -- Skaliert mit Masse
 
+    local function doJump()
+        local vf = SV.myVehicleFolder(); if not vf then notify("Jump","Kein Fahrzeug."); return end
+        if not SV.ensurePrimaryPart(vf) or not vf.PrimaryPart then notify("Jump","Kein PrimaryPart."); return end
+        local pp   = vf.PrimaryPart
+        local mass = math.max(pp.AssemblyMass, 1)
+        pp:ApplyImpulse(Vector3.new(0, J.power * mass, 0))
+        notify("Jump", "Hop!", 1.2)
+    end
+
+    local sec = tab:AddSection({ Name = "Vehicle Jump" })
+    sec:AddButton({ Name = "Vehicle Jump", Callback = doJump })
+    sec:AddSlider({
+        Name = "Jump-Power",
+        Min=80, Max=600, Increment=10, Default=J.power,
+        Callback = function(v) J.power = math.floor(v) end
+    })
+end
