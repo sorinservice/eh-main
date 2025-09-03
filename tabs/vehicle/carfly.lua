df930238831af4209735ee027d179ed3c3e3aac9
sorinservice <nemesisofnocturn@gmail.com>
2025-09-03 22:34:21 +0200

    Update carfly.lua

--- tabs/vehicle/carfly.lua
+++ tabs/vehicle/carfly.lua
@@ -1,1 +1,5 @@
-
+-- tabs/vehicle/carfly.lua
+return function(SV, tab, OrionLib)
+    local sec = tab:AddSection({ Name = "Car Fly" })
+    sec:AddLabel("Derzeit deaktiviert (Server-Desync/AntiCheat).")
+end
