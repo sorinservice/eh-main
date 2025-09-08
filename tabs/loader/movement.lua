6a0feb1fc415b13427395213ccdc4873d6f80fc7
sorinservice <nemesisofnocturn@gmail.com>
2025-09-08 08:28:07 +0200

    Update movement.lua

--- tabs/loader/movement.lua
+++ tabs/loader/movement.lua
@@ -1,6 +1,4 @@
 -- tabs/movement.lua
--- SorinHub - Movement Tab (UI 0.1..1.0 mapped to internal 0.8..5.0)
-
 return function(tab, OrionLib)
     
     local Players      = game:GetService("Players")
@@ -49,7 +47,7 @@ return function(tab, OrionLib)
     ----------------------------------------------------------------
     -- UI mapping config
     local UI_MIN, UI_MAX     = 0.1, 1.0          -- what the user sees
-    local MUL_MIN, MUL_MAX   = 0.8, 5.0          -- internal effective multiplier range
+    local MUL_MIN, MUL_MAX   = 0.8, 6.0          -- internal effective multiplier range
 
     -- linear remap helper: x in [a1..a2] -> [b1..b2]
     local function remap(x, a1, a2, b1, b2)
@@ -233,11 +231,4 @@ return function(tab, OrionLib)
             task.defer(function() if ch then setPartsCollide(ch, false) end end)
         end
     end, CONNS)
-
-    -- optional unload hook:
-    -- on(SomeUnloadSignal, function()
-    --     stopSlide()
-    --     stopNoclip()
-    --     disconnectAll(CONNS)
-    -- end)
 end
